2f9d28edaf810c8970b37cc6164fb0aab66102ca
Iain <iain@zora.co>
2022-01-07 02:56:00 -0500

    [welcome world] init

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,50 +1,58 @@
-import { BigInt } from "@graphprotocol/graph-ts"
+import { CreatedEdition } from "../generated/SingleEditionMintableCreator/SingleEditionMintableCreator";
+import { NFTEdition, TransactionInfo } from "../generated/schema";
+import { NFTEdition as NFTEditionTemplate } from "../generated/templates";
 import {
-  SingleEditionMintableCreator,
-  CreatedEdition
-} from "../generated/SingleEditionMintableCreator/SingleEditionMintableCreator"
-import { ExampleEntity } from "../generated/schema"
+  NFTEdition as NFTEditionContract,
+  OwnershipTransferred,
+  PriceChanged,
+} from "../generated/templates/NFTEdition/NFTEdition";
+import { ethereum } from "@graphprotocol/graph-ts";
+
+function makeTransaction(txn: ethereum.Event): string {
+  const txnInfo = new TransactionInfo(txn.transaction.hash.toHex());
+  txnInfo.block = txn.block.number;
+  txnInfo.timestamp = txn.block.timestamp;
+  txnInfo.save();
+
+  return txnInfo.id;
+}
 
 export function handleCreatedEdition(event: CreatedEdition): void {
-  // Entities can be loaded from the store using a string ID; this ID
-  // needs to be unique across all entities of the same type
-  let entity = ExampleEntity.load(event.transaction.from.toHex())
+  const newEdition = new NFTEdition(
+    event.params.editionContractAddress.toHex()
+  );
+  newEdition.address = event.params.editionContractAddress;
+  newEdition.editionSize = event.params.editionSize;
+  newEdition.editionId = event.params.editionId;
+  newEdition.creator = event.transaction.from;
 
-  // Entities only exist after they have been saved to the store;
-  // `null` checks allow to create entities on demand
-  if (!entity) {
-    entity = new ExampleEntity(event.transaction.from.toHex())
+  newEdition.txn = makeTransaction(event);
 
-    // Entity fields can be set using simple assignments
-    entity.count = BigInt.fromI32(0)
-  }
+  NFTEditionTemplate.create(event.params.editionContractAddress);
 
-  // BigInt and BigDecimal math are supported
-  entity.count = entity.count + BigInt.fromI32(1)
+  newEdition.save();
+}
 
-  // Entity fields can be set based on event parameters
-  entity.editionId = event.params.editionId
-  entity.creator = event.params.creator
+export function handleInitEdition(event: OwnershipTransferred): void {
+  const edition = NFTEdition.load(event.address.toHex())!;
 
-  // Entities can be written to the store with `.save()`
-  entity.save()
+  const editionContract = NFTEditionContract.bind(event.address);
 
-  // Note: If a handler doesn't require existing field values, it is faster
-  // _not_ to load the entity from the store. Instead, create it fresh with
-  // `new Entity(...)`, set the fields that should be updated and save the
-  // entity back to the store. Fields that were not set or unset remain
-  // unchanged, allowing for partial updates to be applied.
+  const uris = editionContract.getURIs();
+  edition.animationURI = uris.value0;
+  edition.animationHash = uris.value1;
+  edition.imageURI = uris.value2;
+  edition.imageHash = uris.value3;
 
-  // It is also possible to access smart contracts from mappings. For
-  // example, the contract that has emitted the event can be connected to
-  // with:
-  //
-  // let contract = Contract.bind(event.address)
-  //
-  // The following functions can then be called on this contract to access
-  // state variables and other data:
-  //
-  // - contract.createEdition(...)
-  // - contract.getEditionAtId(...)
-  // - contract.implementation(...)
+  edition.name = editionContract.name();
+  edition.symbol = editionContract.symbol();
+  edition.salePrice = editionContract.salePrice();
+
+  edition.save();
+}
+
+export function handleEditionPriceChanged(event: PriceChanged): void {
+  const edition = NFTEdition.load(event.address.toHex())!;
+  edition.salePrice = event.params.amount;
+  edition.save();
 }
